e7604428473a10e9f341a8b00c5f2cc6dd76a290
jbcodeforce <jerome.boyer@gmail.com>
2024-08-27 10:48:47 -0700

    work on graph with tool

--- owl-agent-backend/src/athena/llm/agents/base_tool_graph_agent.py
+++ owl-agent-backend/src/athena/llm/agents/base_tool_graph_agent.py
@@ -1,19 +1,101 @@
 import logging
+import json
 from athena.llm.agents.agent_mgr import OwlAgentAbstractRunner, OwlAgent
 from athena.llm.tools.tool_mgr import OwlToolEntity
-from typing import Annotated, Any, Optional
+from typing import Annotated, Any, Optional, Literal
 from typing_extensions import TypedDict
 
 from langchain_core.prompts import BasePromptTemplate
 from langchain_core.runnables.config import RunnableConfig
-from langchain_core.messages import AnyMessage
-
+from langchain_core.messages import AnyMessage, ToolMessage, HumanMessage
+from langgraph.graph import StateGraph, END
 from langgraph.graph.message import add_messages
-from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.checkpoint.memory import MemorySaver
 from langgraph.pregel.types import StateSnapshot
 from langgraph.graph import StateGraph
 
 LOGGER = logging.getLogger(__name__)
 
+class AgentState(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
 
+
+class BasicToolNode:
+    """A node that runs the tools requested in the last AIMessage.
+    return the new state for the graph
+    """
+    def __init__(self, tools: list) -> None:
+        # build a dict tool name: tool
+        self.tools_by_name = {tool.name: tool for tool in tools}
+
+    def __call__(self, inputs: dict):
+        if messages := inputs.get("messages", []):
+            message = messages[-1]
+        else:
+            raise ValueError("No message found in input")
+        outputs = []
+        for tool_call in message.tool_calls:
+            tool_result = self.tools_by_name[tool_call["name"]].invoke(
+                tool_call["args"]
+            )
+            outputs.append(
+                ToolMessage(
+                    content=json.dumps(tool_result),
+                    name=tool_call["name"],
+                    tool_call_id=tool_call["id"],
+                )
+            )
+        return {"messages": outputs}
+    
 class BaseToolGraphAgent(OwlAgentAbstractRunner):
+
+    def __init__(self, agentEntity: OwlAgent, prompt: Optional[BasePromptTemplate], tool_instances: Optional[list[OwlToolEntity]]):
+        self.agent_id = agentEntity.agent_id
+        self.llm=super().instantiate_llm(agentEntity, prompt,tool_instances)
+        tool_node=BasicToolNode(tool_instances)
+        graph_builder = StateGraph(AgentState)
+        graph_builder.add_node("llm", self.call_llm)
+        graph_builder.add_node("tools", tool_node)
+        graph_builder.add_conditional_edges(
+            "llm",
+            self.route_tools,
+            {"tools": "tools", END: END}
+        )
+        graph_builder.add_edge("tools", "llm")
+        graph_builder.set_entry_point("llm")
+        self.graph = graph_builder.compile(checkpointer=MemorySaver())
+
+    def call_llm(self, state: AgentState):
+        """return the new state for the graph"""
+        messages = state['messages']
+        #msg = state["input"]
+        LOGGER.debug(f"@@@ > call_chatbot in Base Graph Assistant: {messages}")
+        message = self.llm.invoke(messages)
+        return {'messages': [message]}
+    
+
+    def invoke(self, request, thread_id: str) -> dict[str, Any] | Any:
+        self.config = {"configurable": {"thread_id": thread_id}}
+        m=HumanMessage(content=request["input"])
+        resp= self.graph.invoke({"messages": [m]}, self.config)
+        return  resp["messages"][-1].content
+         
+    def get_state(self) -> StateSnapshot:
+        return self.graph.get_state(self.config)
+    
+    
+    def route_tools(self,
+        state: AgentState,
+    ) -> Literal["tools", "__END__"]:
+        """Use in the conditional_edge to route to the ToolNode if the last message
+
+        has tool calls. Otherwise, route to the end."""
+        if isinstance(state, list):
+            ai_message = state[-1]
+        elif messages := state.get("messages", []):
+            ai_message = messages[-1]
+        else:
+            raise ValueError(f"No messages found in input state to tool_edge: {state}")
+        if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+            return "tools"
+        return END
--- owl-agent-backend/tests/ut/test_conversation_with_tool.py
+++ owl-agent-backend/tests/ut/test_conversation_with_tool.py
@@ -12,37 +12,10 @@ from athena.llm.conversations.conversation_mgr import get_or_start_conversation
 class TestConversationWithTool(unittest.TestCase):
     """
     Validate conversation with tool to get news from search
-    """
-    def test_base_agent_with_openai_tool_chain(self):
-        """
-        Validate conversation with history. Baseagent uses openai agent with a prompt with chat history
-        """
+    """    
+    def test_conversation_with_chain_tool_agent(self):
         cc = ConversationControl()
-        cc.agent_id="base_openai_tool_agent"
-        cc.user_id="unit_test"
-        cc.thread_id="1"
-        cc.chat_history=[]
-        cc.query="Hi, I'm Bob and my last name is TheBuilder."
-        
-        rep = get_or_start_conversation(cc)
-        assert rep
-        assert rep.message
-        
-        print(f"agent --> {rep.message}") 
-        
-        cc.chat_history=rep.chat_history
-        cc.query="What is my last name?"
-        print(f"Continue the conversation with  --> {cc}") 
-        rep = get_or_start_conversation(cc)
-        print(f"agent --> {rep}")
-        assert rep
-        assert rep.chat_history
-        assert rep.message
-        assert "last name is TheBuilder" in rep.message
-        
-    def test_conversation_with_base_tool_agent(self):
-        cc = ConversationControl()
-        cc.agent_id="base_openai_tool_agent"
+        cc.agent_id="openai_tool_chain"
         cc.user_id="unit_test"
         cc.thread_id="1"
         cc.chat_history=[]
