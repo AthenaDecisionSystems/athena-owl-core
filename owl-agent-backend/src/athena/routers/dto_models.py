51c5a9d2dc007d747ffcee7f0f12163a08269e9b
Jean-Christophe Jardinier <jcjardinier@gmail.com>
2024-08-28 19:14:45 +0200

    Comment out redundant fields

--- owl-agent-backend/src/athena/routers/dto_models.py
+++ owl-agent-backend/src/athena/routers/dto_models.py
@@ -74,9 +74,9 @@ class ResponseChoice(BaseModel):
     choice: str = ""
 
 
-class ConversationModeEnum(str, Enum):
-    open_question = 'OpenQuestion'
-    closed_question = 'ClosedQuestion'
+#class ConversationModeEnum(str, Enum):
+#    open_question = 'OpenQuestion'
+#    closed_question = 'ClosedQuestion'
 
 
 
@@ -102,10 +102,6 @@ class ResponseControl(BaseModel):
 
     status: int = 200
    
-    message: Optional[str] = ''
-    status: int = 200
-    type: ConversationModeEnum = ConversationModeEnum.open_question
-    
     error: Optional[str] = ''
     chat_history: List[ChatMessage] = []
     user_id: Optional[str] = ""
