173904bc02a3971904d342c63b7650a493765bd0
Joël Milgram <joel@milgram.fr>
2024-11-11 16:56:31 +0100

    Add a proxy to backend

--- owl-agent-interface/next.config.js
+++ owl-agent-interface/next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
     output: 'standalone',
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:8002/api/:path*',
+            }
+        ]
+    }
 }
